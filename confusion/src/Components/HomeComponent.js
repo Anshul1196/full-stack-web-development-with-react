fdd17a123765201dc848eada5a01af3cb1b01df8
Anshul 1196 <anshul.chhabra@zensar.com>
2020-09-24 17:27:29 +0530

    React Animation Components

--- confusion/src/Components/HomeComponent.js
+++ confusion/src/Components/HomeComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-
+import { FadeTransform } from 'react-animation-components';
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
         return <Loading />;
@@ -13,7 +13,11 @@ function RenderCard({ item, isLoading, errMess }) {
         );
     }
     else return (
-        <Card>
+        <FadeTransform in 
+                transformProps={{
+                    exitTransform:'scale (0.5) translateY(-50%)'
+                }}>
+              <Card>
                     <CardImg src={baseUrl + item.image} alt={item.name} />
                     <CardBody>
                         <CardTitle>{item.name}</CardTitle>
@@ -21,6 +25,7 @@ function RenderCard({ item, isLoading, errMess }) {
                         <CardText>{item.description}</CardText>
                     </CardBody>
                 </Card>
+                </FadeTransform>
                 );
             }
             
