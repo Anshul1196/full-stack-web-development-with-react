cc82af46647270c3f45c053e5597726c410656b7
Anshul 1196 <anshul.chhabra@zensar.com>
2020-09-24 12:12:00 +0530

    React Redux Form revisited

--- confusion/src/Components/ContactComponent.js
+++ confusion/src/Components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle,Breadcrumb,BreadcrumbItem,Button,Label,Input,Col,Row } from "reactstrap";
 import {Link} from 'react-router-dom';
-import { Control,LocalForm,Errors } from 'react-redux-form';
+import { Control,Form,Errors,actions } from 'react-redux-form';
 
 const required= (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -25,6 +25,7 @@ this.setState({
 handleSubmit(values){
 console.log("Current State is:"+JSON.stringify(values));
 alert("Current State is:"+JSON.stringify(values));
+this.props.resetFeedbackForm();
 
 }
 handleBlur =(field) => (evt)=>{
@@ -79,7 +80,7 @@ this.setState({
                         <h3>Send us your feedback</h3>
                     </div>
                     <div className='col-12 col-md-9'>
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                             <Label htmlfor="Firstname" md={2}>
                                 First Name
@@ -187,7 +188,7 @@ this.setState({
                                     <Button type="submit" color="primary" >Send Feedback</Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
---
+++ confusion/src/redux/forms.js
@@ -0,0 +1,9 @@
+export const InitialFeedback={
+    firstname:'',
+    lastname:'',
+    telenum:'',
+    email:'',
+    agree:false,
+    contactType:'Tel',
+    message:''
+}
